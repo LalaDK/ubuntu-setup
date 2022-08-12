b7de2ee9ea52a2b0bf2465d98504f02e17e4e4d1
Mads Eckardt <mads.eckardt@gmail.com>
2022-08-12 22:04:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
 #! /usr/bin/env bash
+
+sudo apt install fzf ripgrep make build-essential tmux git tree htop wget curl clang zsh libsdl2-dev libsdl2-gfx-dev libsdl2-image-dev
+
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
