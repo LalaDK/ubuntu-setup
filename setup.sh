b5eea4af8a07773e078320e623e8ba5d15dadad9
Mads Eckardt <mads.eckardt@gmail.com>
2022-08-21 11:33:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt install fzf ripgrep make build-essential tmux git tree htop wget curl c
 # RVM
 gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
-rvm install 3.0.0
+rvm install 2.7
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
