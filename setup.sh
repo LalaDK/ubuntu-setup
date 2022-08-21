a6e185e942a18953fa8aa9545a8d518de315e279
Mads Eckardt <mads.eckardt@gmail.com>
2022-08-21 09:32:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,15 @@ sudo apt install fzf ripgrep make build-essential tmux git tree htop wget curl c
 
 
 
+# RVM
+gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
+rvm install 3.0.0
+
+# Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
+# Add cargo PATH (.zshrc / .bashrc)
 # export PATH="$HOME/.cargo/bin:$PATH"
+
+gem install solargraph
